3067c9b6aee58d11585ec8a1ccdc7a1ada094bb4
Mohd Saif Ahmed <saifahmed9308@gmail.com>
2025-08-21 11:07:56 +0530

    fixed the vm bug for collecting the flows , also added logs for the error and success Added a sealed class in Vm , that can be used later for better optimistaion

--- app/src/main/java/com/myjar/jarassignment/ui/vm/JarViewModel.kt
+++ app/src/main/java/com/myjar/jarassignment/ui/vm/JarViewModel.kt
@@ -1,5 +1,6 @@
 package com.myjar.jarassignment.ui.vm
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.myjar.jarassignment.createRetrofit
@@ -10,7 +11,17 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
+sealed class UiState{
+    object Loading : UiState()
+    data class Success(val data: List<ComputerItem>) : UiState()
+    data class Error(val message: String) : UiState()
+}
+
 class JarViewModel : ViewModel() {
+
+    companion object{
+        private const val TAG = "JarViewModel"
+    }
 
     private val _listStringData = MutableStateFlow<List<ComputerItem>>(emptyList())
     val listStringData: StateFlow<List<ComputerItem>>
@@ -20,7 +31,15 @@ class JarViewModel : ViewModel() {
 
     fun fetchData() {
         viewModelScope.launch {
-            repository.fetchResults()
+         try {
+             repository.fetchResults().collect { item->
+                 Log.d(TAG, "$item")
+                 _listStringData.value = item
+             }
+         }catch (e: Exception){
+             Log.e(TAG, "Error fetching data", e)
+             e.printStackTrace()
+             }
+         }
         }
     }
-}
